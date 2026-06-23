dd0e2e8479f1c71e2350422456e64b898e89e692
Bertan Aslan <bertanaslan47@gmail.com>
2026-06-23 18:27:15 +0300

    Loading screen kaldirildi ve build versiyonu guncellendi

--- statsAnaliz/ContentView.swift
+++ statsAnaliz/ContentView.swift
@@ -16,61 +16,32 @@ class NetworkMonitor: ObservableObject {
         monitor.start(queue: queue)
     }
 }
-
+//screen kaldırıldı
 struct WebView: UIViewRepresentable {
     let url: URL
-    @Binding var isLoading: Bool
 
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         webView.scrollView.bounces = false
-        webView.navigationDelegate = context.coordinator
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
         return webView
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        webView.load(request)
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, WKNavigationDelegate {
-        var parent: WebView
-
-        init(_ parent: WebView) {
-            self.parent = parent
-        }
-
-        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            parent.isLoading = true
-        }
-
-        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            parent.isLoading = false
-        }
     }
 }
 
 struct ContentView: View {
     @StateObject private var networkMonitor = NetworkMonitor()
-    @State private var isLoading = true
 
     var body: some View {
         ZStack {
             if networkMonitor.isConnected {
-                WebView(url: URL(string: "https://app.statsanaliz.com")!, isLoading: $isLoading)
+                WebView(url: URL(string: "https://app.statsanaliz.com")!)
                     .ignoresSafeArea()
-
-                if isLoading {
-                    ZStack {
-                        Color.white.ignoresSafeArea()
-                        ProgressView()
-                            .scaleEffect(1.5)
-                    }
-                }
             } else {
                 VStack(spacing: 20) {
                     Image(systemName: "wifi.exclamationmark")
